67416a188a339f8e156f058abb7e32f4a40e67a2
iohzrd <iohzrd@protonmail.com>
2024-04-20 10:16:38 -0700

    build.sh clean

--- build.sh
+++ build.sh
@@ -3,13 +3,18 @@
 export ANDROID_HOME=$HOME/Android/Sdk
 export ANDROID_NDK_HOME=$ANDROID_HOME/ndk
 
+if [ $1="clean" ]; then
+    cargo clean
+    gradle clean
+fi
+
 rustup target add aarch64-linux-android
 rustup target add armv7-linux-androideabi
 rustup target add i686-linux-android
 rustup target add x86_64-linux-android
 
 cargo install cargo-ndk
-cargo ndk -t arm64-v8a -t armeabi-v7a -o serialport/src/main/jniLibs/  build --release
+cargo ndk -t arm64-v8a -t armeabi-v7a -o serialport/src/main/jniLibs/  build # --release
 
 # Build the AAR
 gradle build
